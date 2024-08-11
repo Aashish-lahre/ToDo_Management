db9032c2e4b2419bb58b5eeaee05ebdf21295f83
Ashishlahre <ashishlahre009@gmail.com>
2024-08-12 01:44:41 +0530

    auto-generated dart code for NoteModel's TypeAdapter

---
+++ lib/network/models/NoteModel.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'NoteModel.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  NoteModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteModel(
+      title: fields[0] as String,
+      note: fields[5] as String,
+      bookmarked: fields[2] as bool,
+    )
+      ..time = fields[1] as DateTime
+      ..noteLength = fields[3] as int
+      ..titleLength = fields[4] as int;
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteModel obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.time)
+      ..writeByte(2)
+      ..write(obj.bookmarked)
+      ..writeByte(3)
+      ..write(obj.noteLength)
+      ..writeByte(4)
+      ..write(obj.titleLength)
+      ..writeByte(5)
+      ..write(obj.note);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
